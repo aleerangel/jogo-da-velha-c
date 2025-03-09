762706a32c7c06c84c42ae2fb23e775823165894
Alexandre Rangel <familiagamer1054@gmail.com>
2025-03-09 01:53:56 -0300

    projeto inicial

---
+++ velha.c
@@ -0,0 +1,169 @@
+#include <stdio.h> 
+#include <stdlib.h>
+
+#define LINHAS 3
+#define COLUNAS 3
+
+//cria a matriz do tabuleiro como variavel global
+int tabuleiro[3][3];
+
+//cabecalho das funcoes para organizacao
+void iniciaTabuleiro();
+void imprime();
+void jogada();
+void joga(int* vezJogador);
+void verificaAcabou(int* acabou, int jogadas);
+
+int main() {
+    int acabou=0;
+    //preenche as posicoes da matriz
+    iniciaTabuleiro();
+    //armazena a quantidade de jogadas
+    int jogadas=1;
+    //enquanto nao acabou, acontece uma rodada
+    while(acabou==0) {
+        jogada(&jogadas);
+        verificaAcabou(&acabou, jogadas);
+    }
+    
+    
+    return 0;
+}
+
+//preenche a matriz inicial de 1 a 9
+void iniciaTabuleiro() {
+    int posicao=1;
+    for(int i=0; i<LINHAS; i++ ) {
+        for(int j=0; j<COLUNAS; j++) {
+            tabuleiro[i][j]=posicao;
+            posicao++;
+        }
+    }
+    imprime();
+}
+
+//imprime o tabuleiro
+void imprime() {
+    //limpa o terminal antes de exibir o tabuleiro
+    system("cls");
+    for(int i=0; i<LINHAS; i++) {
+        for(int j=0; j<COLUNAS; j++) {
+            //se a posicao for preenchida por 1 a 9, imprime a posicao
+            if(tabuleiro[i][j]<10) {
+                printf("%d ", tabuleiro[i][j]);
+            } 
+            //se a posicao for preenchida por 11, imprime X
+            else if(tabuleiro[i][j]==11) {
+                printf("X ");
+            }
+            //se a posicao for preenchida por 12, imprime 0
+            else if(tabuleiro[i][j]==12) {
+                printf("O ");
+            } 
+            
+        }
+        printf("\n");
+    }
+}
+
+void jogada(int* jogadas) {
+    printf("Proxima jogada!\n");
+    //se o numero da jogada for impar, a vez e do jogador 1 
+    int vezJogador;
+    if((*jogadas)%2!=0) {
+        vezJogador=1;
+    } else {
+        vezJogador=2;
+    }
+    //armazena a jogada
+    joga(&vezJogador);
+
+    //acrescenta 1 ao numero de jogadas
+    (*jogadas)++;
+}
+
+void joga(int* vezJogador) {
+    int pos;
+    //imprime o jogador da vez
+    if((*vezJogador)==1) {
+        printf("Jogador 1 - X\n");
+    } else {
+        printf("Jogador 2 - O\n");
+    }
+    //escaneia a posicao da jogada
+    printf("Digite a posicao (1-9)\n");
+    scanf("%d", &pos);
+    //adiciona X como 11 ou O como 12 no tabuleiro
+    if(pos>=1 && pos<=3) {
+        if((*vezJogador)==1) {
+            tabuleiro[0][pos-1]= 11;
+        } else{
+            tabuleiro[0][pos-1]= 12;
+        }
+    } else if(pos>=4 && pos<=6) {
+        if((*vezJogador)==1) {
+            tabuleiro[1][pos-4]= 11;
+        } else{
+            tabuleiro[1][pos-4]= 12;
+        }
+    } else if(pos>=7 && pos<=9) {
+        if((*vezJogador)==1) {
+            tabuleiro[2][pos-7]= 11;
+        } else{
+            tabuleiro[2][pos-7]= 12;
+        }
+    }
+    //imprime o tabuleiro
+    imprime();
+}
+
+void verificaAcabou(int* acabou, int jogadas) {
+    int ganhador=0;
+    
+    // Verifica linhas
+    for (int i = 0; i < LINHAS; i++) {
+        //se as 3 posicoes forem iguais, retorna 11 ou 12 para o ganhador
+        if (tabuleiro[i][0] == tabuleiro[i][1] && tabuleiro[i][1] == tabuleiro[i][2]) {
+            ganhador=tabuleiro[i][0];
+            (*acabou)=1;
+            break;
+        }
+    }
+
+    // Verifica colunas
+    for (int i = 0; i < COLUNAS; i++) {
+        //se as 3 posicoes forem iguais, retorna 11 ou 12 para o ganhador
+        if (tabuleiro[0][i] == tabuleiro[1][i] && tabuleiro[1][i] == tabuleiro[2][i]) {
+            ganhador=tabuleiro[0][i];
+            (*acabou)=1;
+            break;
+        }
+    }
+
+    // Verifica diagonal principal (\)
+    //se as 3 posicoes forem iguais, retorna 11 ou 12 para o ganhador
+    if (tabuleiro[0][0] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][2]) {
+        ganhador=tabuleiro[0][0];
+        (*acabou)=1;
+    }
+
+    // Verifica diagonal secundária (/)
+    //se as 3 posicoes forem iguais, retorna 11 ou 12 para o ganhador
+    if (tabuleiro[0][2] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][0]) {
+        ganhador=tabuleiro[0][2];
+        (*acabou)=1;
+    }
+
+    //imprime o vencedor, caso tenha
+    if(ganhador==11) {
+        printf("Jogador 1 ganhou!\n");
+    } else if(ganhador==12) {
+        printf("Jogador 2 ganhou!\n");
+    }
+
+    //se der empate (9 jogadas sem vencedor)
+    if(jogadas==9) {
+        (*acabou)=1;
+        printf("Empate!\n");
+    }
+}
